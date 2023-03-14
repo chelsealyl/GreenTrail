37500d7f313322acb841b991bb1fdb4c4198ec2d
baijingwen <z5306726@ad.unsw.edu.au>
2023-03-15 01:52:16 +1100

    Delete

--- app/src/main/java/com/example/myapplication/PlantIntroduceActivity.java
+++ app/src/main/java/com/example/myapplication/PlantIntroduceActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MonmentsFragment extends AppCompatActivity {
+public class PlantIntroduceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monmentsfragment);
+        setContentView(R.layout.activity_plant_introduce);
     }
 }
